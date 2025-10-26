17ab936489c204059b3c3e1d8680d88405ae5ea8
Aseef Imran <aimran@redhat.com>
2025-10-25 22:06:54 -0400

    corrected mistake in rebase
    corrected mistake in rebase
    
    corrected mistake in rebase

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 # See https://github.com/kubevirt/kubevirt/blob/main/docs/custom-rpms.md
 export KUBEVIRT_CRI=docker  # tooling is VERY flimsy with podman
 # Compile with QEMU and Libvirt with tdx support
-export DOCKER_TAG=tdx-test-31;
+export DOCKER_TAG=tdx-test-40;
 export DOCKER_PREFIX=quay.io/rh-ee-aimran;
-#make generate;
-#make CUSTOM_REPO=tdx-repo.yaml SINGLE_ARCH="x86_64" LIBVIRT_VERSION=0:10.10.0-6.el9s.tdx QEMU_VERSION=17:9.1.0-25.el9s.tdx rpm-deps;
-make push && make manifests;
+sudo -E make generate;
+sudo -E make rpm-deps;
+sudo -E make push && sudo -E make manifests;
 kubectl apply -f _out/manifests/release/kubevirt-operator.yaml
 #kubectl apply -f  _out/manifests/release/kubevirt-cr.yaml
 oc delete vm tdx1
